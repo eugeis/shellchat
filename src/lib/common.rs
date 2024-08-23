b94dee40b1627257cc5867abb3a6d2669723325e
Eugen Eisler <eoeisler@gmail.com>
2024-08-23 22:50:10 +0200

    test: implement unit tests

--- src/lib/common.rs
+++ src/lib/common.rs
@@ -15,3 +15,27 @@ pub struct Error {
     pub message: String,
     pub code: Option<u16>, // You can include an error code if applicable
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+
+    #[test]
+    fn test_question_serialization() {
+        let question = Question {
+            os: "Linux".to_string(),
+            shell: "bash".to_string(),
+            prompt: "What is Rust?".to_string(),
+            explain: false,
+        };
+
+        let json = serde_json::to_string(&question).unwrap();
+        let deserialized_question: Question = serde_json::from_str(&json).unwrap();
+
+        assert_eq!(question.os, deserialized_question.os);
+        assert_eq!(question.shell, deserialized_question.shell);
+        assert_eq!(question.prompt, deserialized_question.prompt);
+        assert_eq!(question.explain, deserialized_question.explain);
+    }
+}
